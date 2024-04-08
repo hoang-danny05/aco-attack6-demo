bcc862d75f9caab22124befce1bd4cbd6fcc207a
hoang-danny05 <bajanboss132@gmail.com>
2024-04-07 20:54:33 -0700

    added all the sql to the script, styled the homepage some more

--- init.sql
+++ init.sql
@@ -1,5 +1,21 @@
+CREATE USER 'target'@'localhost' IDENTIFIED BY 'kali';
+
+CREATE DATABASE demodb;
+
+GRANT ALL PRIVILEGES ON demodb.* TO 'target'@'localhost';
+
+USE DATABASE demodb;
+
+CREATE TABLE logins (
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  password VARCHAR(50)
+);
+
 INSERT INTO logins (id, name, password)
 VALUES 
   (1, "admin", "supersecret11"),
   (2, "superuser", "whitetankmountains"),
   (3, "backupadmin", "mandoandbabyyoga");
+
+SELECT * FROM logins;
